c14bc97f8c0874fda4b4d687cbaedec7be588c58
lekernel <sebastien.bourdeauducq@lekernel.net>
2010-10-26 22:24:36 +0200

    Increase redraw granularity

--- dope-embedded/lib/sched.c
+++ dope-embedded/lib/sched.c
@@ -43,7 +43,7 @@ int init_simple_scheduler(struct dope_services *d);
 
 extern int dope_client_main(int argc, char **argv);
 
-int config_redraw_granularity = 100*1000;
+int config_redraw_granularity = 350*1000;
 
 struct dope_services *dope_services;
 
